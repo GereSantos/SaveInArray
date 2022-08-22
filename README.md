d9daa428e26aaaaab567fd39274fa97df26cffbc
Geremias Santos <Geremias99999@gmail.com>
2022-08-22 16:50:31 -0300

    Insert in array

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Sobre o Mini- Projeto
+
+Laço de repetição que usa a variável I para armazenar
+ os valores que a mesma é passada em cada sessão do for, armazenando a mesma usa
+ um Array , para armazena esse número salvo e passa-lo para o mesmo.
+
+## Linguagens Utilizadas
+
+- JavaScript
---
+++ script.js
@@ -0,0 +1,11 @@
+// Configuração
+const myArray = [];
+
+/*laço de repetição que usa a variável I para armazenar
+ os valores que a mesma é passada em cada sessão do for, armazenando a mesma usa
+ um Array (myArray), para armazena esse número salvo e passa-lo para o mesmo. */
+for(let i =1; i < 11; i+=2){
+  myArray.push(i);
+}
+
+console.log(myArray)
